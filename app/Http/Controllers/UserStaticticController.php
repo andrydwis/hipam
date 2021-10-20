ebeb551fc9544989ceb98769dcb94be44e0031fc
andrydwis <andry.dwi.s@gmail.com>
2021-10-20 13:23:38 +0700

    fixing

--- app/Http/Controllers/UserStaticticController.php
+++ app/Http/Controllers/UserStaticticController.php
@@ -12,7 +12,7 @@ class UserStaticticController extends Controller
     //
     public function index(Request $request)
     {
-        $clients = Client::whereRaw('client_id::text ILIKE %' . $request->keyword . '%')->orWhere('name', 'ilike', '%' . $request->keyword . '%')->with('usages.bill')->paginate(10);
+        $clients = Client::whereRaw('"client_id"::text ilike %' . $request->keyword . '%')->orWhere('name', 'ilike', '%' . $request->keyword . '%')->with('usages.bill')->paginate(10);
 
         $data = [
             'keyword' => $request->keyword,
