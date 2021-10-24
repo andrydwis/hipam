3f44ee1a79263afb9b8eeca20b6c233e5bcd66bd
andrydwis <andry.dwi.s@gmail.com>
2021-10-24 23:10:58 +0700

    fixing

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,7 +48,7 @@
         <section class="vh-lg-100 mt-5 mt-lg-0 bg-soft d-flex align-items-center">
             <div class="container">
                 <div class="d-flex justify-content-center mb-5">
-                    <a href="{{route('user-statistic.index')}}" class="btn btn-primary">Statistik Pelanggan</a>
+                    <a href="{{route('user-statistic.index')}}" class="btn btn-primary">Cek Tagihan Pelanggan</a>
                 </div>
                 <div class="row justify-content-center form-bg-image" style="background-image: url({{asset('assets/img/illustrations/signin.svg')}});">
                     <div class="col-12 d-flex align-items-center justify-content-center">
