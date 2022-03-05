c32e6b52cff168ade0c2339c752ab546927ba97d
Andry <andry.dwi.s@gmail.com>
2022-03-05 07:50:58 +0700

    fix responsive

--- resources/views/my-activity/show.blade.php
+++ resources/views/my-activity/show.blade.php
@@ -20,14 +20,14 @@
 <div class="card">
     <div class="card-header">
         <div class="form-group row">
-            <label for="tanggal_kegiatan" class="col-sm-2 col-form-label">Tanggal Kegiatan</label>
-            <div class="col-sm-10">
+            <label for="tanggal_kegiatan" class="col-6 col-sm-2 col-form-label">Tanggal Kegiatan</label>
+            <div class="col-6 col-sm-10">
                 <input type="date" name="tanggal_kegiatan" class="form-control-plaintext px-3" placeholder="Tanggal Kegiatan" value="{{$activity->created_at->format('Y-m-d')}}" readonly>
             </div>
         </div>
         <div class="form-group row mt-3">
-            <label for="jenis_kegiatan" class="col-sm-2 col-form-label">Jenis Kegiatan</label>
-            <div class="col-sm-10">
+            <label for="jenis_kegiatan" class="col-6 col-sm-2 col-form-label">Jenis Kegiatan</label>
+            <div class="col-6 col-sm-10">
                 <input type="text" name="jenis_kegiatan" class="form-control-plaintext px-3" placeholder="Jenis kegiatan" value="{{$activity->activity_type}}" readonly>
             </div>
         </div>
@@ -35,8 +35,8 @@
     @if($activity->activity_type != 'Pasang baru' && $activity->activity_type != 'Perbaikan saluran pengguna')
     <div class="card-body">
         <div class="form-group row">
-            <label for="jenis_pekerjaan" class="col-sm-2 col-form-label">Jenis Pekerjaan</label>
-            <div class="col-sm-10">
+            <label for="jenis_pekerjaan" class="col-6 col-sm-2 col-form-label">Jenis Pekerjaan</label>
+            <div class="col-6 col-sm-10">
                 <input type="text" name="jenis_pekerjaan" class="form-control-plaintext px-3" placeholder="Jenis pekerjaan" value="{{$activity->job_type}}" readonly>
             </div>
         </div>
@@ -62,26 +62,26 @@
     @if($activity->activity_type == 'Pasang baru' || $activity->activity_type == 'Perbaikan saluran pengguna')
     <div class="card-body">
         <div class="form-group row">
-            <label for="jenis_pekerjaan" class="col-sm-2 col-form-label">Jenis Pekerjaan</label>
-            <div class="col-sm-10">
+            <label for="jenis_pekerjaan" class="col-6 col-sm-2 col-form-label">Jenis Pekerjaan</label>
+            <div class="col-6 col-sm-10">
                 <input type="text" name="jenis_pekerjaan" class="form-control-plaintext px-3" placeholder="Jenis pekerjaan" value="{{$activity->job_type}}" readonly>
             </div>
         </div>
         <div class="form-group row mt-3">
-            <label for="no_surat_tugas" class="col-sm-2 col-form-label">No Surat Tugas</label>
-            <div class="col-sm-10">
+            <label for="no_surat_tugas" class="col-6 col-sm-2 col-form-label">No Surat Tugas</label>
+            <div class="col-6 col-sm-10">
                 <input type="text" name="no_surat_tugas" class="form-control-plaintext px-3" placeholder="No Surat Tugas" value="{{$activity->assignment_letter_number}}" readonly>
             </div>
         </div>
         <div class="form-group row mt-3">
-            <label for="nama_pelanggan" class="col-sm-2 col-form-label">Nama Pelanggan</label>
-            <div class="col-sm-10">
+            <label for="nama_pelanggan" class="col-6 col-sm-2 col-form-label">Nama Pelanggan</label>
+            <div class="col-6 col-sm-10">
                 <input type="text" name="nama_pelanggan" class="form-control-plaintext px-3" placeholder="Nama Pelanggan" value="{{$activity->name}}" readonly>
             </div>
         </div>
         <div class="form-group row mt-3">
-            <label for="rt_rw" class="col-sm-2 col-form-label">RT / RW</label>
-            <div class="col-sm-10">
+            <label for="rt_rw" class="col-6 col-sm-2 col-form-label">RT / RW</label>
+            <div class="col-6 col-sm-10">
                 <input type="text" name="rt_rw" class="form-control-plaintext px-3" placeholder="RT / RW" value="{{$activity->rt_rw}}" readonly>
             </div>
         </div>
@@ -91,7 +91,7 @@
             @php
             $photos = json_decode($activity->photo);
             @endphp
-            <div class="col-sm-10 d-flex gap-3">
+            <div class="col-sm-10 d-flex flex-wrap gap-3">
                 @foreach($photos as $photo)
                 <img src="{{asset('storage/'.$photo)}}" alt="Foto" class="img-thumbnail" style="object-fit: cover;" width="250" height="250">
                 @endforeach
